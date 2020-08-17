9be7120c23b5fcb745d4cc691bcf3d31f7a2b5c2
nurf21 <nurfauzangiri@gmail.com>
2020-08-17 08:12:39 +0700

    :card_file_box: Updated database and postman collection

--- etc/pos_app.sql
+++ etc/pos_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2020 at 11:17 PM
+-- Generation Time: Aug 17, 2020 at 03:09 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -39,7 +39,9 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_name`, `category_created_at`, `category_updated_at`) VALUES
-(1, 'Drinks', '2020-08-12 13:08:19', '0000-00-00 00:00:00');
+(1, 'Drinks', '2020-08-12 13:08:19', '0000-00-00 00:00:00'),
+(3, 'Desserts', '2020-08-13 03:55:51', '0000-00-00 00:00:00'),
+(4, 'Foods', '2020-08-13 03:56:17', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -49,10 +51,20 @@ INSERT INTO `category` (`category_id`, `category_name`, `category_created_at`, `
 
 CREATE TABLE `history` (
   `history_id` int(11) NOT NULL,
-  `invoice` int(6) NOT NULL,
+  `history_invoice` int(6) NOT NULL,
   `history_subtotal` int(15) NOT NULL,
   `history_created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`history_id`, `history_invoice`, `history_subtotal`, `history_created_at`) VALUES
+(61, 946643, 227700, '2020-08-17 00:49:10'),
+(62, 188569, 195800, '2020-08-17 00:50:35'),
+(63, 920857, 821700, '2020-08-17 00:51:39'),
+(66, 462616, 138600, '2020-08-17 01:08:02');
 
 -- --------------------------------------------------------
 
@@ -64,9 +76,23 @@ CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL,
   `history_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `qty` int(6) NOT NULL,
-  `total_price` int(15) NOT NULL
+  `order_qty` int(6) NOT NULL,
+  `order_total_price` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `history_id`, `product_id`, `order_qty`, `order_total_price`) VALUES
+(96, 61, 10, 3, 207000),
+(97, 62, 6, 6, 168000),
+(98, 62, 4, 2, 10000),
+(99, 63, 1, 9, 90000),
+(100, 63, 3, 7, 105000),
+(101, 63, 10, 8, 552000),
+(106, 66, 5, 2, 66000),
+(107, 66, 7, 2, 60000);
 
 -- --------------------------------------------------------
 
@@ -90,7 +116,15 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `product_price`, `category_id`, `product_created_at`, `product_updated_at`, `product_status`) VALUES
-(1, 'Espresso', '#', 10000, 1, '2020-08-12 15:15:23', '0000-00-00 00:00:00', 1);
+(1, 'Espresso', '#', 10000, 1, '2020-08-12 15:15:23', '0000-00-00 00:00:00', 1),
+(3, 'Coffee Latte', '#', 15000, 1, '2020-08-13 07:01:26', '0000-00-00 00:00:00', 1),
+(4, 'Cappuccino', '#', 5000, 1, '2020-08-13 07:01:41', '0000-00-00 00:00:00', 1),
+(5, 'Red Velvet Latte', '#', 33000, 1, '2020-08-13 07:02:12', '0000-00-00 00:00:00', 1),
+(6, 'Choco Rum', '#', 28000, 3, '2020-08-13 07:02:28', '2020-08-13 07:03:17', 1),
+(7, 'Black Forest', '#', 30000, 3, '2020-08-13 07:03:48', '0000-00-00 00:00:00', 1),
+(8, 'Chicken Katsu Dabu-dabu', '#', 60000, 4, '2020-08-13 07:04:07', '0000-00-00 00:00:00', 1),
+(9, 'Salmon Truffle Teriyaki', '#', 60000, 4, '2020-08-13 07:04:22', '0000-00-00 00:00:00', 1),
+(10, 'Wiener Schnitzel', '#', 69000, 4, '2020-08-13 07:04:35', '0000-00-00 00:00:00', 1);
 
 --
 -- Indexes for dumped tables
@@ -128,25 +162,25 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
