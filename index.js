dac29006897290bcd4577c1a7d7df38d67b5c09a
nurf21 <nurfauzangiri@gmail.com>
2020-09-07 21:19:19 +0700

    :see_no_evil: Added .env file to .gitignore

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-// Import dependencies
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-
-// Import route from src
 const routerNavigation = require('./src')
-
-// Declare variable app to use express
 const app = express()
 
-// Middleware
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(express.static('uploads'))
 app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Origin', process.env.IP)
   response.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization')
   next()
 })
 app.use('/', routerNavigation)
-
-// Error handling when path is wrong
 app.get('*', (request, response) => {
   response.status(404).send('Path Not Found !')
 })
 
-// Run express on host: 127.0.0.1 and port: 3001
-app.listen(3001, '127.0.0.1', () => {
-  console.log('Express app is listening on host: 127.0.0.1 and port: 3001')
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log(`Express app is listening on host: ${process.env.IP} and port: ${process.env.PORT}`)
 })
